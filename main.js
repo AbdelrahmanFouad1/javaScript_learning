37e0b91813a7e81afe2a69437b84ae255bc7df15
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2022-08-21 09:14:12 +0200

    Arrow Function

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const isMarried = false;
 age = 30;
 
 
-function summarizeUser(userName, userAge, userIsMarried){
+const summarizeUser = (userName, userAge, userIsMarried) => {
     return 'Name is ' + userName + ', age is ' + userAge + ' and isMarried is ' + userIsMarried;
 }
+
+const add = (a, b) => a + b;
+
+console.log(add(1, 2));
 
 console.log(summarizeUser(name, age, isMarried));
