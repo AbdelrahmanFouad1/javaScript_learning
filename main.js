1b599265dc812b436a2824c5e0a98d01ec56c3b9
Abd El Rahman Fouad <abdofouad.cs@gmail.com>
2022-08-21 18:14:15 +0200

    destructing

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+const person = {
+    name: 'Alex',
+    age: 30,
+    isMarried : false,
+    greet (){
+        console.log('Hello '+ this.name);
+    }
+};
+
+const {name, age} = person;
+console.log(name, age);
+
+// person.greet();
+
 const hobbies = ['sports', 'cooking'];
 
-hobbies.push('programming');
-console.log(hobbies);
+const [hoby1, hoby2] = hobbies;
+console.log(hoby1, hoby2);
+
+// hobbies.push('programming');
+// console.log(hobbies);
